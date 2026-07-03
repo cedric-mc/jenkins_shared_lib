d0ece9c1b47f96959562046298296a5231af11ed
Cédric MARIYA CONSTANTINE <cedric.mariya.constantine@gmx.fr>
2026-07-03 16:53:48 +0200

    Ajout d'une vérification de l'installation du plugin Slack avant l'envoi de notifications

--- vars/sendSlack.groovy
+++ vars/sendSlack.groovy
@@ -2,6 +2,10 @@ def call(String status) {
     def color = (status == 'SUCCESS') ? '#00FF00' : '#FF0000'
     def message = "Build #${env.BUILD_NUMBER} de ${env.JOB_NAME} - Statut : ${status}"
     
-    // Utilise le plugin Slack de Jenkins
-    slackSend(channel: '#ci-cd', color: color, message: message)
+    // Vérifie si le plugin Slack est installé avant de l'appeler
+    if (metaClass.getMetaMethod("slackSend", Map) || this.respondsTo("slackSend", Map)) {
+        slackSend(channel: '#ci-cd', color: color, message: message)
+    } else {
+        echo "📢 [Notification] Plugin Slack absent du serveur. Message : ${message}"
+    }
 }
